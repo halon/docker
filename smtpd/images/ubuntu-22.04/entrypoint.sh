e34d3862d75ed0ee4b8b6cfe78900e2ec617e34b
Fredrik Lengstrand <fredrik.lengstrand@halon.se>
2026-06-24 09:13:15 +0000

    Follow restricted pod security standards

--- smtpd/images/ubuntu-22.04/entrypoint.sh
+++ smtpd/images/ubuntu-22.04/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
 
-/opt/halon/bin/halonctl license fetch --username ${HALON_REPO_USER} --password ${HALON_REPO_PASS} --path /license.key
+/opt/halon/bin/halonctl license fetch --username ${HALON_REPO_USER} --password ${HALON_REPO_PASS} --path /var/run/halon/license.key
 
 exec "$@"
